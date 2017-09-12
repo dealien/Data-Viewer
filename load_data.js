625341f1ec12321b953fd1b03fecbc5db8dee92c
dealien <dealien@users.noreply.github.com>
2017-09-12 12:30:52 -0400

    Update load_data.js

--- load_data.js
+++ load_data.js
@@ -2,12 +2,14 @@ var debug_mode = true;
 var students;
 var theTable = "";
 
-function load_data() {
-  
+function load_data(data) {
+  console.log("students", students);
+  console.log("data", data);
 }
 
 document.onload = $.getJSON("student_data.json", function(loaded_data) {
-    console.log(loaded_data); 
-load_data();});
+    console.log(loaded_data);
+  students = loaded_data;
+load_data(loaded_data);});
 
 
