ffdbe2611c015098fbe3ba4a0b73473529ed1275
dealien <dealien@users.noreply.github.com>
2017-09-12 12:29:13 -0400

    Update load_data.js

--- load_data.js
+++ load_data.js
@@ -7,7 +7,7 @@ function load_data() {
 }
 
 document.onload = $.getJSON("student_data.json", function(loaded_data) {
-    console.log(loaded_data); // this will show the info it in firebug console
+    console.log(loaded_data); 
 load_data();});
 
 
