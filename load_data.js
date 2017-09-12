58f33503de2f7d6ef37b676b7e530a906c6eb143
dealien <dealien@users.noreply.github.com>
2017-09-12 12:28:37 -0400

    Update load_data.js

--- load_data.js
+++ load_data.js
@@ -2,37 +2,11 @@ var debug_mode = true;
 var students;
 var theTable = "";
 
-function table_gen(ar) {
-  console.info('running table_gen()');
-  theTable = "";
-  if (debug_mode == true) {
-    console.log("Starting table_gen()");
-    console.log(ar);
-    console.log("table width:", ar[0].length);
-    console.log("table length:", ar.length);
-  }
-  for (var j = 0; j < ar[0].length; j++) {
-    theTable += "<tr>";
-    for (var k = 0; k < ar.length; k++) {
-      theTable += "<td>" + ar[j][k] + "</td>";
-      if (debug_mode == true) {
-        console.info("row " + j + ", column " + k)
-        // console.log(theTable);}
-      }
-      theTable += "</tr>";
-    }
-    if (debug_mode == true) {
-      // console.log("theTable", theTable);
-    }
-  }
-  return theTable;
-}
-
 function load_data() {
   
 }
 
-document.onload = $.getJSON("test.json", function(json) {
+document.onload = $.getJSON("student_data.json", function(json) {
     console.log(json); // this will show the info it in firebug console
 load_data();});
 
