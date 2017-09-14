04e931ebf8879cee437a8f1c900ffc1c398ad47e
Unknown <dragonofawsome@gmail.com>
2017-09-14 15:22:01 -0400

    Set importer text to selected file name

--- load_data.js
+++ load_data.js
@@ -115,30 +115,33 @@ function import_file() {
   fr.readAsText(files.item(0));
 };
 
-'use strict';
+// Update file loader display on file selection
+function loader_setup() {
+  (function(document, window, index) {
+    var inputs = document.querySelectorAll('.importfile');
+    Array.prototype.forEach.call(inputs, function(input) {
+      var label = input.nextElementSibling,
+        labelVal = label.innerHTML;
 
-(function(document, window, index) {
-  var inputs = document.querySelectorAll('.importfile');
-  Array.prototype.forEach.call(inputs, function(input) {
-    var label = input.nextElementSibling,
-      labelVal = label.innerHTML;
+      input.addEventListener('change', function(e) {
+        var fileName = '';
+        if (this.files && this.files.length > 1)
+          fileName = (this.getAttribute('data-multiple-caption') || '').replace('{count}', this.files.length);
+        else
+          fileName = e.target.value.split('\\').pop();
 
-    input.addEventListener('change', function(e) {
-      var fileName = '';
-      if (this.files && this.files.length > 1)
-        fileName = (this.getAttribute('data-multiple-caption') || '').replace('{count}', this.files.length);
-      else
-        fileName = e.target.value.split('\\').pop();
+        // Display selected file name on importer
+        if (fileName)
+          label.querySelector('span').innerHTML = fileName;
+        else
+          label.innerHTML = labelVal;
+      });
 
-      // Display selected file name on importer
-      if (fileName)
-        label.querySelector('span').innerHTML = fileName;
-      else
-        label.innerHTML = labelVal;
+      // Firefox bug fix
+      input.addEventListener('focus', function() { input.classList.add('has-focus'); });
+      input.addEventListener('blur', function() { input.classList.remove('has-focus'); });
     });
+  }(document, window, 0));
+}
 
-    // Firefox bug fix
-    input.addEventListener('focus', function() { input.classList.add('has-focus'); });
-    input.addEventListener('blur', function() { input.classList.remove('has-focus'); });
-  });
-}(document, window, 0));
+window.onload = loader_setup;
